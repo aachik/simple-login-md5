3c9dfb97a85d5654db19635a07c60d983807efe4
abdul <abdulhamid@outlook.com>
2015-04-21 09:16:32 -0500

    se agrego un usuario acceptable

--- cpa_abs.py
+++ cpa_abs.py
@@ -4,6 +4,8 @@ import hashlib
 app = Flask(__name__)
 
 
+app.secret_key = 'nydf0^3bxnlrt_rx03l$!#14q8dbr=q%))2a-2t@07_0n%%*m$'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
@@ -12,9 +14,12 @@ def index():
 		m = m.hexdigest()
 		print(request.form['email'])
 		print(m)
-
-		data={'m': m, 'password': request.form['password']}
-		return render_template('success.html', data=data)
+		if request.form['password'] == 'aa121292':
+			data={'m': m, 'password': request.form['password']}
+			return render_template('success.html', data=data)
+		else:
+			data={'m': m, 'password': request.form['password']}
+			flash('password encriptado:{}, password sin encriptar:{}'.format(m, request.form['password']))
 	return render_template('index.html')
 
 
