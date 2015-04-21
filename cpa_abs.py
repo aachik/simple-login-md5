fad22e8c6e6c2ed46922043b88f249aced0f975b
abdul <abdulhamid@outlook.com>
2015-04-21 08:43:47 -0500

    deleted some code that i generated automatically and i wanst using

--- cpa_abs.py
+++ cpa_abs.py
@@ -4,8 +4,6 @@ import hashlib
 app = Flask(__name__)
 
 
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
@@ -18,12 +16,6 @@ def index():
 		data={'m': m, 'password': request.form['password']}
 		return render_template('success.html', data=data)
 	return render_template('index.html')
-
-
-@app.route('/data')
-def names():
-    data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
-    return jsonify(data)
 
 
 if __name__ == '__main__':
